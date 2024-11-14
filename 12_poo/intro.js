7ddb28e3d62b5b9bcbe27109981e6f5461eae1ee
Jason-Brum <contato@jasonb.com.br>
2024-11-14 08:57:12 -0300

    Ok

--- 12_poo/intro.js
+++ 12_poo/intro.js
@@ -1,19 +1,8 @@
-class Produto {
-    constructor(nome, quantidadeEstoque) {
-        console.log("construtor chamado")
-        this.nome = nome
-        this.quantidadeEstoque
-       
-    }
+/*Em poo é correto afirmar que os objetos são instâncias da classe principal. A classe é como se fosse um projeto, um molde. Tudo o que for feito nessa classe será replicado em todos os produtos. No entanto se quiser alterar só um produto, é possível também. Cada objeto é a instância de uma classe.
 
-    comprar(quantidade){
-        this.quantidadeEstoque -= quantidade
-    }
-    adicionar(quantidade) {
-        this.quantidadeEstoque += quantidade
+Uma classe determina características e comportamentos
 
-    }
-}
+#Criar uma classe de produto que receba na função construtora o nome e a quantidade em estoque inicial. Implementar 2 métodos: comprar() e adicionar(). Fazer todas as verificações possíveis para evitar erros. */
 
-const caneta = new Produto("caneta", 10)
-const lapis = new Produto("lapis", 20)
+class
+
